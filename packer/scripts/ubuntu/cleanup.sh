53acae2d79fe7c5aecb7707f2d54267c199ab9cb
Josh Nichols <technicalpickles@github.com>
2014-12-09 10:12:18 -0500

    Don't uninstall dev things, as that ended up removing node and npm

--- packer/scripts/ubuntu/cleanup.sh
+++ packer/scripts/ubuntu/cleanup.sh
@@ -1,10 +1,11 @@
 #!/bin/bash -eux
 #
 # this removes specific linux kernels, such as
-# linux-image-3.11.0-15-generic but 
+# linux-image-3.11.0-15-generic but
 # * keeps the current kernel
 # * does not touch the virtual packages, e.g.'linux-image-generic', etc.
 #
+set -x
 dpkg --list | awk '{ print $2 }' | grep 'linux-image-3.*-generic' | grep -v `uname -r` | xargs apt-get -y purge
 
 # delete all linux headers
@@ -14,10 +15,10 @@ dpkg --list | awk '{ print $2 }' | grep linux-headers | xargs apt-get -y purge
 dpkg --list | awk '{ print $2 }' | grep linux-source | xargs apt-get -y purge
 
 # delete development packages
-dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
+#dpkg --list | awk '{ print $2 }' | grep -- '-dev$' | xargs apt-get -y purge
 
 # delete compilers and other development tools
-apt-get -y purge cpp gcc g++
+#apt-get -y purge cpp gcc g++
 
 # delete X11 libraries
 apt-get -y purge libx11-data xauth libxmuu1 libxcb1 libx11-6 libxext6
